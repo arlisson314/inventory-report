ab3d41f4ba8eefea8d03008191c70acbcbe77283
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-12 16:06:00 -0500

    test: implemta testes ao metodo init da classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,36 +1,29 @@
 from inventory_report.inventory.product import Product
 
 
-# producMock = {
-#     "id": 1,
-#     "nome_do_produto": "Lama",
-#     "nome_da_empresa": "vale",
-#     "data_de_fabricacao": "01/06/2022",
-#     "data_de_validade": "01/06/2023",
-#     "numero_de_serie": "1234567",
-#     "instrucoes_de_armazenamento": "conservar longe de crianças do cão",
-# }
+producMock = {
+    "id": 1,
+    "nome_do_produto": "Lama",
+    "nome_da_empresa": "Vale",
+    "data_de_fabricacao": "01/06/2022",
+    "data_de_validade": "01/06/2023",
+    "numero_de_serie": "1234567",
+    "instrucoes_de_armazenamento": "conservar longe de crianças do cão",
+}
 
 
 def test_cria_produto():
 
-    produc = Product(
-        1,
-        "Lama",
-        "Vale",
-        "01/06/2022",
-        "01/06/2023",
-        "1234567",
-        "conservar longe de crianças do cão",
-    )
+    """** espalha o conteudo do producMock na instancia de Product"""
+    produc = Product(**producMock)
 
-    assert produc.id == 1
-    assert produc.nome_do_produto == "Lama"
-    assert produc.nome_da_empresa == "Vale"
-    assert produc.data_de_fabricacao == "01/06/2022"
-    assert produc.data_de_validade == "01/06/2023"
-    assert produc.numero_de_serie == "1234567"
+    assert produc.id == producMock["id"]
+    assert produc.nome_do_produto == producMock["nome_do_produto"]
+    assert produc.nome_da_empresa == producMock["nome_da_empresa"]
+    assert produc.data_de_fabricacao == producMock["data_de_fabricacao"]
+    assert produc.data_de_validade == producMock["data_de_validade"]
+    assert produc.numero_de_serie == producMock["numero_de_serie"]
     assert (
         produc.instrucoes_de_armazenamento
-        == "conservar longe de crianças do cão"
+        == producMock["instrucoes_de_armazenamento"]
     )
