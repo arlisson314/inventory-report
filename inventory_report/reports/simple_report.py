9e7151c74405aeb4c832733ca2dfdc1c2d364ab4
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-12 17:35:18 -0500

    feat: implementa metodo statico para gerar um relatorio simples na classe SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,20 @@
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(products_list):
+        data_de_fabricacao = [
+            key["data_de_fabricacao"] for key in products_list
+        ]
+
+        data_de_validade = [key["data_de_validade"] for key in products_list]
+
+        empresas = [key["nome_da_empresa"] for key in products_list]
+        nome_da_empresa = Counter(empresas).most_common()[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {min(data_de_fabricacao)}\n"
+            f"Data de validade mais próxima: {min(data_de_validade)}\n"
+            f"Empresa com mais produtos: {nome_da_empresa}"
+        )
