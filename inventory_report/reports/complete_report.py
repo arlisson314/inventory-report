7af0cf3779e182d7d7d047ae3b5415f200434793
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-13 10:13:38 -0500

    feat: implementa classmethod para gerar um relatorio completo na classe CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,19 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, products_list):
+        report = super().generate(products_list)
+
+        empresas = [key["nome_da_empresa"] for key in products_list]
+
+        estoque = Counter(empresas).most_common()
+
+        report += "\nProdutos estocados por empresa:\n"
+
+        for value in estoque:
+            report += f"- {value[0]}: {value[1]}\n"
+
+        return report
