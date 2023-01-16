f1fc7a5d7c3e820aca8971feab293e5c2ca80ca1
Arlisson N. Costa <arlisson314@gmail.com>
2023-01-16 15:15:24 -0500

    test: adiciona testes para a o modulo tests/product_report/test_product_report.py

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,26 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+producMock = {
+    "id": 1,
+    "nome_do_produto": "Lama",
+    "nome_da_empresa": "Vale",
+    "data_de_fabricacao": "01/06/2022",
+    "data_de_validade": "01/06/2023",
+    "numero_de_serie": "1234567",
+    "instrucoes_de_armazenamento": "conservar longe de crianças do cão",
+}
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produc = Product(**producMock)
+    produc_data = str(produc)
+
+    assert (isinstance(produc_data, str)) is True
+
+    assert (produc_data) == (
+        f"O produto {produc.nome_do_produto}"
+        f" fabricado em {produc.data_de_fabricacao}"
+        f" por {produc.nome_da_empresa} com validade"
+        f" até {produc.data_de_validade}"
+        f" precisa ser armazenado {produc.instrucoes_de_armazenamento}."
+    )
