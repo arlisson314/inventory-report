180a3b7849dbd3dac7082d4885efef5a04d24568
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-07 19:42:45 -0300

    test: implementa test_decorar_relatorio

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,30 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+
+    list_mock = [
+        {
+            "id": "1",
+            "nome_do_produto": "Nicotine Polacrilex",
+            "nome_da_empresa": "Target Corporation",
+            "data_de_fabricacao": "2021-02-18",
+            "data_de_validade": "2023-09-17",
+            "numero_de_serie": "CR25 1551 4467 2549 4402 1",
+            "instrucoes_de_armazenamento": "instrucao 1",
+        }
+    ]
+
+    data_antiga = "Data de fabricação mais antiga:"
+    data_validade = "Data de validade mais próxima:"
+    empresa = "Empresa com mais produtos:"
+    data_a = "2021-02-18"
+    data_v = "2023-09-17"
+    emp = "Target Corporation"
+
+    report = ColoredReport(SimpleReport).generate(list_mock)
+
+    assert f"\033[32m{data_antiga}\033[0m \033[36m{data_a}\033[0m" in report
+    assert f"\033[32m{data_validade}\033[0m \033[36m{data_v}\033[0m" in report
+    assert f"\033[32m{empresa}\033[0m \033[31m{emp}\033[0m" in report
