4d9a82eea222285411082696ce21367a9baae45b
Arlisson N. Costa <arlisson314@gmail.com>
2023-03-07 19:43:24 -0300

    style: commenta classe CsvImporter

---
+++ inventory_report/importer/csv_importer.py
@@ -0,0 +1,14 @@
+# from inventory_report.importer.importer import Importer
+# import csv
+
+
+# class CsvImporter(Importer):
+#     @classmethod
+#     def import_data(cls, file_path):
+
+# # extension = file_path.split(".")[1]
+# cls.raise_extension_error(extension, "csv")
+
+# with open(file_path, mode="r") as file:
+#     content = list(csv.DictReader(file))
+# return content
